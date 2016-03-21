2fbcfedbdadceaf3a710f5adc924b00d039593ad
Nicolas Martin <nicolas.martin.0986@gmail.com>
2016-03-20 20:36:51 -0400

    commented mysql code until we know how to use it

--- scoreUpdate.go
+++ scoreUpdate.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -63,14 +62,59 @@ func main() {
 	fmt.Printf("%d - %d \r\n", feed.LiveData.LineScore.Teams.Home.Goals, feed.LiveData.LineScore.Teams.Away.Goals)
 	fmt.Println(feed)
 
-	db, err := sql.Open("mysql", "root:password@/ScoreBot")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-
-	for {
-
-	}
+	// db, err := sql.Open("mysql", "root:password@/ScoreBot")
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// defer db.Close()
+	//
+	// for {
+	//
+	// }
 
 }
+
+// func getUnReadMessages(db *sql.DB) (incomingMessage, error) {
+// 	stmtOut, err := db.Prepare("SELECT id, author_id, thread_id, author_name, message, isRead, timeReceived FROM inbox WHERE isRead = ?")
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+//
+// 	defer stmtOut.Close()
+//
+// 	var message incomingMessage
+//
+// 	errNoRow := stmtOut.QueryRow(0).Scan(&message.ID, &message.AuthorID, &message.ThreadID, &message.AuthorName, &message.Message, &message.isRead)
+// 	// if err != nil {
+// 	// 	panic(err.Error())
+// 	// }
+//
+// 	return message, errNoRow
+// }
+//
+// func toggleIsRead(db *sql.DB, message incomingMessage) {
+// 	stmUpdateInbox, err := db.Prepare("update inbox set isRead = 1 where id = ?;")
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+//
+// 	defer stmUpdateInbox.Close()
+// 	_, err = stmUpdateInbox.Exec(message.ID)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// }
+//
+// func insertMessageToSend(db *sql.DB, message incomingMessage, returnMessage string) {
+// 	stmNewOutbox, err := db.Prepare("INSERT INTO `outbox` (`author_id`, `thread_id`, `author_name`, `message`) values (?, ?, ?, ?)")
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+//
+// 	defer stmNewOutbox.Close()
+// 	fmt.Printf("returnMessage = %s \r\n", returnMessage)
+// 	_, err = stmNewOutbox.Exec(message.AuthorID, message.ThreadID, message.AuthorName, returnMessage)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// }
